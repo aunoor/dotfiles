c4ef3120ded498c52a4199df85feec858c33ac3f
Dmitry Komissarov <aunoor@gmail.com>
2019-07-02 13:16:59 +0300

    Separate work and home configs

--- .sawfish/startup.sh
+++ .sawfish/startup.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
 ~/.fehbg &
-/usr/bin/polybar top &
+
+. ~/.station
+case $STATION in
+    "work")
+	~/bin/polybar work &
+	~/bin/Telegram &
+	/opt/Rocket.Chat/rocketchat-desktop &
+	sleep 1
+	~/bin/polybar-msg cmd restart
+	;;
+    "thinkpad")
+	~/bin/polybar thinkpad &
+	;;
+esac
