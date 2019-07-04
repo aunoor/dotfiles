e03d39d02e11e8d2eb941330846e4d2e4ad66d87
Dmitry Komissarov <aunoor@gmail.com>
2019-07-04 20:27:15 +0300

    Configs for home

--- .sawfish/startup.sh
+++ .sawfish/startup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+/usr/lib/polkit-gnome-authentication-agent-1 &
+
 ~/.fehbg &
 
 . ~/.station
@@ -10,8 +12,19 @@ case $STATION in
 	/opt/Rocket.Chat/rocketchat-desktop &
 	sleep 1
 	~/bin/polybar-msg cmd restart
+	/usr/bin/conky -b -m 0
+	/usr/bin/conky -b -m 1
 	;;
     "thinkpad")
+	/usr/bin/xcompmgr -C -n &
 	~/bin/polybar thinkpad &
+	;;
+    "home")
+	/usr/bin/xcompmgr -C -n &
+	~/bin/polybar home &
+	~/bin/Telegram &
+	sleep 1
+	~/bin/polybar-msg cmd restart
+	/usr/bin/conky -b
 	;;
 esac
